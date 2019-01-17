365c9cd67408f381d4f0e7716856cd28e815cc63
Sachin Sanap <sasanap@ngvpn01-173-146.dyn.scz.us.nvidia.com>
2019-01-17 13:47:36 -0500

    Check if new targets file is empty before running any setup steps on them

--- ansible/cleandfc.sh
+++ ansible/cleandfc.sh
@@ -3,6 +3,6 @@ set -o
 ./stopandcleandfc.sh
 echo "Cleaning up targets"
 parallel-ssh -h inventory/targets.txt -i './cleandfcstate.sh'
-parallel-ssh -h inventory/new_targets.txt -i './cleandfcstate.sh'
+if [[ -s inventory/new_targets.txt ]]; then parallel-ssh -h inventory/new_targets.txt -i './cleandfcstate.sh'; fi
 echo "Cleaning up proxy"
 parallel-ssh -h inventory/proxy.txt -i './cleandfcstate.sh'
--- ansible/stopandcleandfc.sh
+++ ansible/stopandcleandfc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 parallel-ssh -h inventory/targets.txt -P 'for dfcpid in `ps -C dfc -o pid=`; do echo Stopping DFC $dfcpid; sudo kill $dfcpid; done'
-parallel-ssh -h inventory/new_targets.txt -P 'for dfcpid in `ps -C dfc -o pid=`; do echo Stopping DFC $dfcpid; sudo kill $dfcpid; done'
+if [[ -s inventory/new_targets.txt ]]; then parallel-ssh -h inventory/new_targets.txt -P 'for dfcpid in `ps -C dfc -o pid=`; do echo Stopping DFC $dfcpid; sudo kill $dfcpid; done'; fi
 parallel-ssh -h inventory/proxy.txt -i 'ps -C dfc -o pid= | xargs sudo kill'
 parallel-ssh -h inventory/proxy.txt -i 'sudo rm -rf /var/log/dfc*'
 parallel-ssh -h inventory/targets.txt -i 'sudo rm -rf /var/log/dfc*'
